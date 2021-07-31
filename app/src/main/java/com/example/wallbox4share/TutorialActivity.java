1d65ca3e90c514927dc0f0e50fe654126eb9bc6a
Catalinu23 <72308620+Catalinu23@users.noreply.github.com>
2021-07-31 18:13:41 +0300

    added login page

--- app/src/main/java/com/example/wallbox4share/TutorialActivity.java
+++ app/src/main/java/com/example/wallbox4share/TutorialActivity.java
@@ -1,20 +1,25 @@
 package com.example.wallbox4share;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class TutorialActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_tutorial);
 
-        Button button = findViewById(R.id.goToTutorialButton);
+        ViewPager viewPager = findViewById(R.id.ViewPager);
+        TutorialSlideshowAdapter adapter = new TutorialSlideshowAdapter(this);
+        viewPager.setAdapter(adapter);
+        Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,8 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void openActivity() {
-        Intent intent = new Intent(this, TutorialActivity.class);
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
-
 }
