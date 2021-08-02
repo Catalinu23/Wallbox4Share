e6e3fb526c392aa7a94a49a2924c88496c2e6ed8
Catalinu23 <72308620+Catalinu23@users.noreply.github.com>
2021-08-02 17:41:45 +0300

    added basic map

--- app/src/main/java/com/example/wallbox4share/LoginActivity.java
+++ app/src/main/java/com/example/wallbox4share/LoginActivity.java
@@ -15,15 +15,29 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button button = findViewById(R.id.goToTutorialButton);
+        Button signInButton = findViewById(R.id.SignInButton);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openActivity();
+                openTutorialActivity();
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMapActivity();
             }
         });
     }
 
-    private void openActivity() {
+    private void openMapActivity() {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    private void openTutorialActivity() {
         Intent intent = new Intent(this, TutorialActivity.class);
         startActivity(intent);
     }
