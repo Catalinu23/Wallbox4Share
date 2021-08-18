c76b969ddb0630c0611fe019c7f3e6e1424fcd24
Catalinu23 <72308620+Catalinu23@users.noreply.github.com>
2021-08-18 13:18:41 +0300

    changed text color on buttons

---
+++ app/src/main/java/com/example/wallbox4share/MyCarActivity.java
@@ -0,0 +1,16 @@
+package com.example.wallbox4share;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_car);
+
+        
+    }
+}
