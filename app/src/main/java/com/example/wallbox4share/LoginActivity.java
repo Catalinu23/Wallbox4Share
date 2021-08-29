7045c45cac71e35f0b81d5d736c97166d278363c
sorana5 <66966645+sorana5@users.noreply.github.com>
2021-08-29 16:18:04 +0300

    i really fixed a bug

--- app/src/main/java/com/example/wallbox4share/LoginActivity.java
+++ app/src/main/java/com/example/wallbox4share/LoginActivity.java
@@ -66,7 +66,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onBackPressed() {
         super.onBackPressed();
-        finish();
+        finishAffinity();
     }
 
 }
