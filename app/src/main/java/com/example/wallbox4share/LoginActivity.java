3dc5b41cc090abdbfeec93992d3cfaac4e34d98e
sorana5 <66966645+sorana5@users.noreply.github.com>
2021-08-29 15:44:28 +0300

    added sign up page

--- app/src/main/java/com/example/wallbox4share/LoginActivity.java
+++ app/src/main/java/com/example/wallbox4share/LoginActivity.java
@@ -4,20 +4,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.gson.Gson;
-
-import org.json.JSONObject;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -32,6 +23,9 @@ public class LoginActivity extends AppCompatActivity {
         Button button = findViewById(R.id.goToTutorialButton);
         Button signInButton = findViewById(R.id.SignInButton);
 
+        TextView goToSignUp = findViewById(R.id.noAccountTextView2);
+        goToSignUp.setMovementMethod(LinkMovementMethod.getInstance());
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,6 +39,13 @@ public class LoginActivity extends AppCompatActivity {
                 openMapActivity();
             }
         });
+
+        goToSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignUpActivity();
+            }
+        });
     }
 
     private void openMapActivity() {
@@ -54,6 +55,11 @@ public class LoginActivity extends AppCompatActivity {
 
     private void openTutorialActivity() {
         Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+    }
+
+    private void openSignUpActivity() {
+        Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
 
